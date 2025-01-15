42e047bab175b214e1e13e1320fc318005621cb0
Lucas Gabriel <100373893+Lukeehz@users.noreply.github.com>
2025-01-14 21:15:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const token = process.env.GOOGLE_TOKEN
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+const genAI = new GoogleGenerativeAI(token);  // Substitua pelo seu token de API
+
+async function run(prompt) {
+  const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash-exp' });
+  const result = await model.generateContent(prompt);
+  return result.response.text || 'Desculpe, não consegui gerar uma resposta.';
+}
+
+module.exports = { run };
